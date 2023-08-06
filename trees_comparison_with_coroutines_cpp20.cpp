e748ea41806c8aec1b8929b46ee776e09a606320
Egor Sergeenko <moonground@gmail.com>
2023-08-06 20:13:56 +0200

    Add project files.

---
+++ trees_comparison_with_coroutines_cpp20.cpp
@@ -0,0 +1,12 @@
+﻿// trees_comparison_with_coroutines_cpp20.cpp : Defines the entry point for the application.
+//
+
+#include "trees_comparison_with_coroutines_cpp20.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
